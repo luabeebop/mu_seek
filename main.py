a40c4a55f2618cf50991d241e752599a75899d6d
ycimatu <ycimatu@xtm-intl.com>
2025-03-21 11:34:46 +0800

    Comments everywhere

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-from tkinter import filedialog
-from tkinter import *
-import pygame
-import os
+from tkinter import filedialog #This is to import the file dialog
+from tkinter import * #This is to import everything from tkinter
+import pygame #This is to import the pygame module
+import os #This is to import the os module
 
 root = Tk() #TK is from tkinter
-root.title("Mu-seek")
-root.geometry("350x450")
+root.title("Mu-seek") #This is to set the title of the window
+root.geometry("350x450") #This is to set the size of the window
 
 pygame.mixer.init() #This is to initialize the pygame mixer
 
 menubar = Menu(root) #This is the menu bar
 root.config(menu=menubar) #This is to configure the menu bar
 
-songs = []
-current_song = ""
-pause = False
+songs = [] #This is the list of songs
+current_song = "" #This is the current song
+pause = False #This is the current state of the music
 
-def load_music():
-    global current_song
+def load_music(): #This is to load the music
+    global current_song #This is to access the current song variable
     root.directory = filedialog.askdirectory() #This is to open the file dialog
 
-    for song in os.listdir(root.directory):
-        name, ext = os.path.splitext(song)
-        if ext == ".mp3":
-            songs.append(song)
+    for song in os.listdir(root.directory): #This is to loop through the files in the directory
+        name, ext = os.path.splitext(song) #This is to get the name and extension of the file
+        if ext == ".mp3": #This is to check if the file is an mp3 file
+            songs.append(song) #This is to add the song to the list of songs
 
-    for song in songs:
-        songlist.insert("end", song)
+    for song in songs: #This is to loop through the songs
+        songlist.insert("end", song) #This is to insert the song into the songlist
 
-    songlist.selection_set(0)
-    current_song = songs[songlist.curselection()[0]]
+    songlist.selection_set(0) #This is to select the first song in the list
+    current_song = songs[songlist.curselection()[0]] #This is to get the current song
 
 # Buttons functionality
 
-def play_music():
-    pass
+def play_music(): #This is to play the music
+    pass #This is to pass the function
 
-def pause_music():
-    pass
+def pause_music(): #This is to pause the music
+    pass #This is to pass the function
 
-def next_music():
-    pass
+def next_music(): #This is to play the next music
+    pass #This is to pass the function
 
-def previous_music():
-    pass
+def previous_music(): #This is to play the previous music
+    pass #This is to pass the function
 
 organize_menu = Menu(menubar, tearoff=False) #This is the organize menu
 menubar.add_cascade(label="Selection", menu=organize_menu) #This is to add the organize menu to the menu bar
@@ -57,7 +57,7 @@ next_btn_image = PhotoImage(file="next.png") #This is to load the next button im
 previous_btn_image = PhotoImage(file="previous.png") #This is to load the previous button image
 
 control_frame = Frame(root) #This is the frame for the control buttons
-control_frame.pack()
+control_frame.pack() #This is to pack the control frame
 
 play_btn = Button(control_frame, image=play_btn_image, borderwidth=0) #This is the play button
 pause_btn = Button(control_frame, image=pause_btn_image, borderwidth=0) #This is the pause button
